f26cc0799116f53adcd09d3e6d113194e2809747
Unknown <kenley.strik@casema.nl>
2020-01-02 16:16:43 +0100

    Small Change

--- app/src/main/java/com/example/eindopdracht_client_side_development_app/views/MapsActivity.java
+++ app/src/main/java/com/example/eindopdracht_client_side_development_app/views/MapsActivity.java
@@ -29,6 +29,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap)
     {
-        googleMap = googleMap;
+        this.googleMap = googleMap;
     }
 }
